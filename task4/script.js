bcea1954cf965653e410b4bbd178d05692a7b502
smart-skill <web.pr@mail.ru>
2023-04-19 18:38:43 +0400

    init version 1.0.1

--- task4/script.js
+++ task4/script.js
@@ -3,7 +3,7 @@ const targetLink = document.querySelector('#targetLink');
 targetLink.onclick = function(event) {
     console.log('Вы нажали на ссылку и вызвали метод prompt()');
     const targetPrompt = prompt('Введите текст чтобы сменить ссылке название');
-    console.log('Вы ввели новое значение ', targetPrompt);
+    console.log('Вы ввели новое значение: ', targetPrompt);
     targetLink.textContent = targetPrompt;
     event.preventDefault();
 };
--- task5/script.js
+++ task5/script.js
@@ -1,13 +1,15 @@
 const input = document.querySelector('input');
 const duplicateField = document.querySelector('#duplicateField');
+const button = document.querySelector('button');
 
-document.querySelector('button').addEventListener('click', (event) => {
-    const inputPrompt = prompt('Введите ваш текст');
-    input.value = inputPrompt;
-    event.preventDefault();
-    duplicateField.textContent = input.value;
-    input.value = '';
-    console.log('Введенный текст: ', inputPrompt ,' получен !');
+
+input.addEventListener('input', function (e) {
+	duplicateField.textContent = input.value;
 });
 
-//
+button.addEventListener('click', (event) => {
+    input.value = null;
+    console.log('Работает! Вы ввели: ', duplicateField.textContent);
+    duplicateField.textContent = null;
+    event.preventDefault();
+});
