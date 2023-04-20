61218e085154f4307b4c038913fc8f1319e47c3d
smart-skill <web.pr@mail.ru>
2023-04-20 06:21:25 +0400

    init version 1.1.0

--- task3/script.js
+++ task3/script.js
@@ -1,59 +1,33 @@
-// ===========================================================================================
-// Ручной вариант:
-// ===========================================================================================
+const green = document.querySelector('#green');
+const yellow = document.querySelector('#yellow');
+const red = document.querySelector('#red');
 
-const manualGreen = document.querySelector('#manualGreen');
-const manualYellow = document.querySelector('#manualYellow');
-const manualRed = document.querySelector('#manualRed');
-
-function mBgGreen() {
-    manualGreen.style.background = ('green');
-    manualYellow.style.background=('black');
-    manualRed.style.background=('black');
-    manualGreen.removeEventListener('click', mBgGreen);
-    manualYellow.addEventListener('click', mBgYellow);
-    manualRed.addEventListener('click', mBgRed);
+function makeGreen() {
+    green.style.background = ('green');
+    yellow.style.background=('black');
+    red.style.background=('black');
+    green.removeEventListener('click', makeGreen);
+    yellow.addEventListener('click', makeYellow);
+    red.addEventListener('click', makeRed);
 }
-manualGreen.addEventListener('click', mBgGreen);
+green.addEventListener('click', makeGreen);
 
-function mBgYellow(){
-    manualGreen.style.background = ('black');
-    manualYellow.style.background=('yellow');
-    manualRed.style.background=('black');
-    manualYellow.removeEventListener('click', mBgYellow);
-    manualRed.addEventListener('click', mBgRed);
-    manualGreen.addEventListener('click', mBgGreen);
+function makeYellow(){
+    green.style.background = ('black');
+    yellow.style.background=('yellow');
+    red.style.background=('black');
+    yellow.removeEventListener('click', makeYellow);
+    red.addEventListener('click', makeRed);
+    green.addEventListener('click', makeGreen);
 }
-manualYellow.addEventListener('click', mBgYellow);
+yellow.addEventListener('click', makeYellow);
 
-function mBgRed(){
-    manualGreen.style.background = ('black');
-    manualYellow.style.background=('black');
-    manualRed.style.background=('red');
-    manualRed.removeEventListener('click', mBgRed);
-    manualGreen.addEventListener('click', mBgGreen);
-    manualYellow.addEventListener('click', mBgYellow);
+function makeRed(){
+    green.style.background = ('black');
+    yellow.style.background=('black');
+    red.style.background=('red');
+    red.removeEventListener('click', makeRed);
+    green.addEventListener('click', makeGreen);
+    yellow.addEventListener('click', makeYellow);
 }
-manualRed.addEventListener('click', mBgRed);
-
-// ===========================================================================================
-// Автоматический вариант:
-// ===========================================================================================
-
-var autoTraffic = document.querySelectorAll('.autoTraffic > div'), 
-	timers = [1000, 1000, 1000], counter = 0;
-
-document.addEventListener('load', start);
-
-var start = setTimeout(function() {
-    traffic();
-}, 100);
-
-function traffic() {
-	for (var i=0; i < autoTraffic.length; i++) {
-		autoTraffic[i].classList.remove('on');
-	}
-	autoTraffic[counter].classList.add('on');
-	setTimeout(traffic, timers[counter]);
-	counter = (counter + 1) % timers.length;
-}
+red.addEventListener('click', makeRed);
